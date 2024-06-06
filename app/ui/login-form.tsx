7d83d39a450503fd557d22ba0973b297f8a0aac1
Ilya <stoynov.oleg@gmail.com>
2024-06-06 17:10:50 +0300

    commit

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -13,6 +13,7 @@ import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+  
   return (
     <form className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -66,12 +67,12 @@ export default function LoginForm() {
           aria-live="polite"
           aria-atomic="true"
         >
-          {errorMessage &&
+          {errorMessage &&(
             <>
             <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
             <p className="text-sm text-red-500">{errorMessage}</p>
             </>
-          }
+          )}
         </div>
       </div>
     </form>
